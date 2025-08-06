4110847c98cbd61aa586e21a45130b0b85a2743e
Emanuelle <emanuellenmoura@gmail.com>
2025-08-06 08:23:36 -0300

    código com sensor infravermelho

--- main.py
+++ main.py
@@ -1,59 +1,87 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, UltrasonicSensor
-from pybricks.parameters import Port, Stop, Direction
+from pybricks.hubs import EV3Brick # type: ignore
+from pybricks.ev3devices import Motor, UltrasonicSensor, InfraredSensor # type: ignore
+from pybricks.parameters import Port, Stop, Direction  # type: ignore
+from time import sleep
+from pybricks.tools import wait # type: ignore
+
+class Enum():
+    """
+    A simple (and fallible) substitute for the enum.Enum class
+    """
+    def __init__(self, cls_name, items):
+        if type(items) == dict:
+            names = items.keys()
+            vals  = items.values()
+        else:
+            names = items
+            vals  = range(len(items))
+        
+        self.name_str_list = names
+        self.constant_map = dict(zip(names, vals))
+        self.name_str_map = dict(zip(vals, names))
+
+
+    def __getattr__(self, name):
+        return self.constant_map[name]
+    
+    def __getitem__(self, item):
+        return self.constant_map[item]
+    def __call__(self, num):
+        return self.name_str_map[num]
+    
+    def __iter__(self):
+        yield from self.name_str_list
+
+    def __contains__(self, val):
+        return (val in self.constant_map or
+                val in self.name_str_map)
+
+    def __len__(self):
+        return len(self.constant_map)
+
+#Estados
+estado = Enum("Estado", ["PARA","ANDA_RETO", "GIRA"])
+
+PARA = estado.PARA
+ANDA_RETO = estado.ANDA_RETO
+GIRA = estado.GIRA
+
+
 
 ev3 = EV3Brick()
 
 motor_esq = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 motor_dir = Motor(Port.B, Direction.COUNTERCLOCKWISE)
-ultra_esq = UltrasonicSensor(Port.S1)
-ultra_dir = UltrasonicSensor(Port.S2)
+infra = InfraredSensor(Port.S1)
 
-# Estados
-PARA = 0
-ANDA_RETO = 1
-GIRA_ESQ = 2
-GIRA_DIR = 3
 
 # Constantes
 VELOCIDADE_MAX  = 1000
-VELOCIDADE_GIRO = 300
-DISTANCIA_MAXIMA  = 650 # Distância máxima para detectar um objeto (em mm)
-DISTANCIA_COMBATE = 125 # Distância mínima para detectar um objeto (em mm) e levantamento do braço
+VELOCIDADE_GIRO = 200
+DISTANCIA_MAXIMA  = 70 # Distância máxima para detectar um objeto (em %)
+
 
 def main():
     ev3.speaker.beep()
     estado = PARA
     while True:
         estado = prox_estado(estado)
-        
         if (estado == ANDA_RETO):
             moverRobo(VELOCIDADE_MAX, VELOCIDADE_MAX)
-        elif (estado == GIRA_ESQ):
+        elif (estado == GIRA):
             moverRobo(VELOCIDADE_GIRO, -VELOCIDADE_GIRO)
-        elif (estado == GIRA_DIR):
-            moverRobo(-VELOCIDADE_GIRO, VELOCIDADE_GIRO)
         else:
             pararRobo()
+        sleep(0.05)
 
 def prox_estado(estado_atual):
-    dist_esq = ultra_esq.distance()
-    dist_dir = ultra_dir.distance()
-
-    if (dist_esq < DISTANCIA_MAXIMA and dist_dir < DISTANCIA_MAXIMA):
-        estado = ANDA_RETO
-    elif (dist_esq < DISTANCIA_MAXIMA):
-        print("ESQ")
-        estado = GIRA_ESQ
-    elif (dist_dir < DISTANCIA_MAXIMA):
-        print("DIR")
-        estado = GIRA_DIR
+    distancia = infra.distance()
+    if distancia <= 70:
+        return ANDA_RETO
     else:
-        estado = estado_atual
-
-    return estado
-
+        return GIRA
+    
 def moverRobo(vel_esq, vel_dir):
     motor_esq.run(vel_esq)
     motor_dir.run(vel_dir)
